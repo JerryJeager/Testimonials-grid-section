e0b342f7678b523edab013aa1304d806e8c14e90
Amadi Jerry <amadijerry310@gmail.com>
2023-03-21 08:25:10 +0100

    completedd mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "moderate-violet": "hsl(263, 55%, 52%)",
+        "dark-grayish-blue": "hsl(217, 19%, 35%)",
+        "dark-blackish-blue": "hsl(219, 29%, 14%)",
+        "light-grey": "hsl(0, 0%, 81%)",
+        "light-greyish-blue": "hsl(210, 46%, 95%)"
+      }
+    },
   },
   plugins: [],
 }
